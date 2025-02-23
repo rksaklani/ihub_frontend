f3ef480aa3b315108921ac600d51102c59ffbc0c
Rk Saklani <rksaklani90@gmail.com>
2025-02-23 23:52:17 +0530

    add the url

--- src/components/consts/ApiUriConst.ts
+++ src/components/consts/ApiUriConst.ts
@@ -1,6 +1,6 @@
-export const apiBaseUrl =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_API_URL
-    : "http://localhost:5000";
+export const apiBaseUrl ="https://ihubinv.netlify.app/"
+//   process.env.NODE_ENV === "production"
+//     ? process.env.REACT_APP_API_URL
+//     : "http://localhost:5000";
 
 
